9d870e5010d41dc430ee0a77a96c67cbcb2c594c
Oleksandr Moskalenko <43320447+thelesik@users.noreply.github.com>
2022-05-01 15:47:49 -0500

    Update 1-version-userinputfile.sh

--- 1-version-userinputfile.sh
+++ 1-version-userinputfile.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+aws_access_key_id="";
+aws_secret_access_key="";
+aws_output_format="json";
+region1="us-east-1";
+region2="us-east-2";
+keyname="${region1}DefaultKeyfortaskName1233211233211233122312321";
+keyname2="${region2}DefaultKeyfortaskName123321123321121213123321";
+#serverdata=$(echo serverdata.txt);
